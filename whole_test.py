e244a8d2061e4a0b7420432c2850ad96ed8f3f62
EricCTsao <uy34567890@gmail.com>
2021-12-03 16:17:14 +0800

    extract experimental info using different method and try to plot 2D mapping of E-degree and its 2nd derivative

--- whole_test.py
+++ whole_test.py
@@ -1,53 +1,101 @@
 import numpy as np
 import re
+import matplotlib.pyplot as plt
+
+#purpose: read the file and generate the figure (from NSRRC U9 ARPES beamline) 
 
 #data pathway
-path_input = "20180815_0001_mod.txt"
+path_input = "20180815_0001.txt"
 path_output = "test.txt"
 
-sample_name = []
-theta = []
-phi = []
-temp = []
-kinetic_energy = []
-angle = []
-delay_time = []
+#generate the list with each line of the txt file
+ListofData = []
+
+def extract_data(path):
+     with open (path, "r") as f:
+          for line in f:
+              Line_split = line.split('\n')[0]
+              ListofData.append(Line_split)
+          return ListofData
+
+#modify the list since there is one space in the file, so remove it (the remaining one is delete in ".split('\n')")
+Data_list = extract_data(path_input)
+Data_list.pop()
+
+#extract useful information from the file
+d1_size = [int(Data_list[7]) for Data_list[7] in re.findall(r'-?\d+\.?\d*', Data_list[7])][-1]
+kinetic_energy = [float(Data_list[8]) for Data_list[8] in re.findall(r'\d*[.]\d*', Data_list[8])]
+
+d2_size = [int(Data_list[10]) for Data_list[10] in re.findall(r'-?\d+\.?\d*', Data_list[10])][-1]
+angle = [float(Data_list[11]) for Data_list[11] in re.findall(r'\d*[.]\d*', Data_list[11])]
+
+comments = [float(Data_list[38]) for Data_list[38] in re.findall(r'-?\d+\.?\d*', Data_list[38])]
+theta = comments[0]
+phi = comments[1]
+temp = comments[-1]
+
+#generate 2D matrix with 
+value_list = []
+
+def generate_value_list():
+     for i in Data_list[47:]:
+          i_new = list(map(float, i.split()))
+          del i_new[0]
+          value_list.append(i_new)
+     return value_list
+
+real_value_list = generate_value_list()
+real_value_list_mx = np.array (real_value_list)
+
+print(real_value_list_mx.shape)
+
+#try to get second derivatives of the matrix
+def hessian(x):
+    """
+    Calculate the hessian matrix with finite differences
+    Parameters:
+       - x : ndarray
+    Returns:
+       an array of shape (x.dim, x.ndim) + x.shape
+       where the array[i, j, ...] corresponds to the seco nd derivative x_ij
+    """
+    x_grad = np.gradient(x) 
+    hessian = np.empty((x.ndim, x.ndim) + x.shape, dtype=x.dtype) 
+    for k, grad_k in enumerate(x_grad):
+        # iterate over dimensions
+        # apply gradient again to every component of the first derivative.
+        tmp_grad = np.gradient(grad_k) 
+        for l, grad_kl in enumerate(tmp_grad):
+            hessian[k, l, :, :] = grad_kl
+    return hessian
 
 
-#read the file (from NSRRC U9 ARPES beamline) 
-with open (path_input, "r") as f:
-    for line in f:
-        #print(line)
-        #if 'Region Name=SnSe'
-        if "Dimension 1 size" in line: 
-             s = [int(s) for s in re.findall(r'-?\d+\.?\d*', line)]
-             d1_size = s[-1]
-             #print(d1_size)
-        if "Dimension 2 size" in line: 
-             s = [int(s) for s in re.findall(r'-?\d+\.?\d*', line)]
-             d2_size = s[-1]
-             #print(d2_size)
-        if "Dimension 1 scale" in line:
-             kinetic_energy = [float(kinetic_energy) for kinetic_energy in re.findall(r'\d*[.]\d*', line)]
-             #print (kinetic_energy)
-        if "Dimension 2 scale" in line:
-             angle = [float(angle) for angle in re.findall(r'\d*[.]\d*', line)]
-             #print (angle)
-        if "Comments=" in line:
-             s = [float(s) for s in re.findall(r'-?\d+\.?\d*', line)]
-             theta = s[0]
-             phi = s[1]
-             temp = s[-1]
-             #print(theta)
-             #print(phi)
-             #print(temp)
+second_dev = hessian(real_value_list_mx)[0, 0, :, :, ]
+print(second_dev)
+
+plt.imshow(second_dev, plt.cm.gray)
+plt.colorbar()
+plt.show()
+
+#print(value_list)
+#print(len(value_list))
+#print(Data_list[47].split())
+#print(len(Data_list[47].split()))
+#print(type(Data_list[47]))
 
 
-with open (path_input, "r") as f:
-    for line in f.readlines(2):
-        print(line)
-             #value = [float(value) for value in re.findall(r'\d*[.]\d*', line)]
-             #print(value)
+
+
+#print(d1_size)
+#print(kinetic_energy)
+#print(type(kinetic_energy))
+#print(len(kinetic_energy))
+#print(type(len(kinetic_energy)))
+#rint(theta)
+#print(phi)
+#print(temp)
+
+
 
 #generate 2D matrix with coordinates
 
